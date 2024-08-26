6b3cbe63de03e7b79b770e87abcd5c83631045ac
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-25 22:17:54 -0600

    moo

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-   if username == "admin" and password == "12345":
+   if username == "admin" and password == "12345" or username == "ADMIN" and password == "12345":
     return ("Access granted")
    else:
-    return ("Access denied")
+      return ("Access denied")
+
 
 def hows_the_weather(temperature):
-    # your code here
+
     pass
 
 def fizzbuzz(num):
