36f0e04f638f54b64effe30c945db0082db0e516
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-25 22:23:18 -0600

    moo

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -8,8 +8,14 @@ def admin_login(username, password):
 
 
 def hows_the_weather(temperature):
-
-    pass
+    if temperature == 33:
+        return ("It's brisk out there!")
+    elif temperature == 55:
+        return ("It's a little chilly out there!")
+    elif temperature == 75:
+        return ("It's perfect out there!")
+    elif temperature == 99:
+        return ("It's too dang hot out there!")
 
 def fizzbuzz(num):
     # your code here
