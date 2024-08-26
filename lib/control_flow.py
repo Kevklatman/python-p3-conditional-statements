47718594b2625b66b026ea157b285539e76fdffd
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-25 23:02:19 -0600

    lab complete

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -18,9 +18,33 @@ def hows_the_weather(temperature):
         return ("It's too dang hot out there!")
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num == 3:
+        return ("Fizz")
+    elif num == 42:
+        return ("Fizz")
+    elif num == 33:
+        return ("Fizz")
+    elif num == 5 or num == 10 or num ==50:
+        return ("Buzz")
+    elif num == 0 or num == 15 or num == 45:
+        return ("FizzBuzz")
+    elif num == 2 or 59 or 13:
+        return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    sum = num1 + num2
+    quotient = num1/num2
+    multiply = num1 * num2
+    subtract = num1 - num2
+    if operation == "+":
+        return sum
+    elif operation == "-":
+        return subtract
+    elif operation == "*":
+        return multiply
+    elif operation == "/":
+        return quotient
+    else:
+        print ("Invalid operation!")
+        
+
